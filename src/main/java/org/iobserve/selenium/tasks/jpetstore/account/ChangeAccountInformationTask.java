d6abbeda5b394e388223ed012fa9ebe4a2ece0eb
Marc Adolf <mad@informatik.uni-kiel.de>
2018-03-16 19:29:23 +0100

    more attributes in the account changing task

--- src/main/java/org/iobserve/selenium/tasks/jpetstore/account/ChangeAccountInformationTask.java
+++ src/main/java/org/iobserve/selenium/tasks/jpetstore/account/ChangeAccountInformationTask.java
@@ -19,31 +19,81 @@ import org.iobserve.selenium.tasks.AbstractUserTask;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Task to change a given user information. Requires the user to be logged in.
+ *
+ * @author Marc Adolf
+ *
+ */
 public class ChangeAccountInformationTask extends AbstractUserTask {
+    private final Attribute attribute;
 
-    public ChangeAccountInformationTask() {
-        // TODO Auto-generated constructor stub
+    public ChangeAccountInformationTask(final Attribute attributToChange) {
+        this.attribute = attributToChange;
     }
 
     @Override
     public void executeTask(final WebDriver driver, final String baseUrl) {
-        final String attribute = "account.firstName";
+        // TODO
         final String newValue = "Joe";
 
-        driver.get(baseUrl + "/jpetstore/actions/Account.action?editAccountForm");
-        driver.findElement(By.name(attribute)).clear();
-        driver.findElement(By.name(attribute)).sendKeys(newValue);
-        driver.findElement(By.name(attribute)).click();
+        String attributeString = "";
 
-        // driver.findElement(By.name("account.lastName")).click();
-        // driver.findElement(By.name("account.email")).click();
-        // driver.findElement(By.name("account.phone")).click();
-        // driver.findElement(By.name("account.address1")).click();
-        // driver.findElement(By.name("account.address2")).click();
-        // driver.findElement(By.name("account.city")).click();
-        // driver.findElement(By.name("account.state")).click();
-        // driver.findElement(By.name("account.zip")).click();
-        // driver.findElement(By.name("account.country")).click();
+        // Ausreichend?
+        final boolean checkBox = false;
+
+        switch (this.attribute) {
+        case FIRSTNAME:
+            attributeString = "account.firstName";
+            break;
+
+        case LASTNAME:
+            attributeString = "account.lastName";
+            break;
+
+        case EMAIL:
+            attributeString = "account.email";
+            break;
+
+        case PHONE:
+            attributeString = "account.phone";
+            break;
+
+        case ADDRESS1:
+            attributeString = "account.address1";
+            break;
+
+        case ADDRESS2:
+            attributeString = "account.address2";
+            break;
+
+        case CITY:
+            attributeString = "account.city";
+            break;
+
+        case STATE:
+            attributeString = "account.state";
+            break;
+
+        case ZIP:
+            attributeString = "account.zip";
+            break;
+
+        case COUNTRY:
+            attributeString = "account.country";
+            break;
+
+        default:
+            break;
+        }
+
+        if (!checkBox) {
+            driver.get(baseUrl + "/jpetstore/actions/Account.action?editAccountForm");
+            driver.findElement(By.name(attributeString)).clear();
+            driver.findElement(By.name(attributeString)).sendKeys(newValue);
+            driver.findElement(By.name(attributeString)).click();
+        }
+
         // driver.findElement(By.name("account.languagePreference")).click();
         // driver.findElement(By.xpath("//option[@value='english']")).click();
         // driver.findElement(By.name("account.languagePreference")).click();
@@ -82,6 +132,16 @@ public class ChangeAccountInformationTask extends AbstractUserTask {
     public String getName() {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    /**
+     * The possible changeable fields in the account form.
+     *
+     * @author Marc Adolf
+     *
+     */
+    public enum Attribute {
+        FIRSTNAME, LASTNAME, EMAIL, PHONE, ADDRESS1, ADDRESS2, CITY, STATE, ZIP, COUNTRY
     }
 
 }
